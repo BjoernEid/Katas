4ee54a0bd1a4fc796fc7303d3f72140c4c146ee4
Bjoern <bjoerneid@googlemail.com>
2023-02-27 18:25:08 +0100

    Add description

--- test/anagrams/AnagramTest.java
+++ test/anagrams/AnagramTest.java
@@ -1,8 +1,13 @@
 package anagrams;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//Anagrams cyber-dojo 编程操练 Write a program to generate all potential anagrams of an input string.
+//
+//        For example, the potential anagrams of "biro" are
+//
+//        biro bior brio broi boir bori
+//        ibro ibor irbo irob iobr iorb
+//        rbio rboi ribo riob roib robi
+//        obir obri oibr oirb orbi orib
 
 public class AnagramTest {
 
