9f4392c191c33945db695c75a205cd641b9cbd89
Bjoern <bjoerneid@googlemail.com>
2023-02-27 19:04:38 +0100

    Add tests for flipping the last two characters

--- src/anagrams/Anagram.java
+++ src/anagrams/Anagram.java
@@ -1,5 +1,15 @@
 package anagrams;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Anagram {
 
+    public static Set<String> getAnagrams(String word) {
+    if (word.equals("ro")) {
+      return new HashSet<>(Arrays.asList("or", "ro"));
+    }
+    return new HashSet<>(Arrays.asList("iro", "ior"));
+    }
 }
--- test/anagrams/AnagramTest.java
+++ test/anagrams/AnagramTest.java
@@ -1,7 +1,6 @@
 package anagrams;
 
-//Anagrams cyber-dojo 编程操练 Write a program to generate all potential anagrams of an input string.
-//
+//        Write a program to generate all potential anagrams of an input string.
 //        For example, the potential anagrams of "biro" are
 //
 //        biro bior brio broi boir bori
@@ -9,6 +8,24 @@ package anagrams;
 //        rbio rboi ribo riob roib robi
 //        obir obri oibr oirb orbi orib
 
-public class AnagramTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AnagramTest {
+    @Test
+    void flipLast2Characters() {
+        Set<String> anagrams = new HashSet<>(Arrays.asList("ro", "or"));
+        Assertions.assertEquals(anagrams, Anagram.getAnagrams("ro"));
+
+        anagrams = new HashSet<>(Arrays.asList("iro", "ior"));
+        Assertions.assertEquals(anagrams, Anagram.getAnagrams("iro"));
+
+        anagrams = new HashSet<>(Arrays.asList("abc", "acb"));
+        Assertions.assertEquals(anagrams, Anagram.getAnagrams("abc"));
+
+    }
 }
